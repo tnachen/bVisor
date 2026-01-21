17fd8cdff9c2e654e2dce25c6a83b1599a000825
erik-dunteman <erikdunteman@gmail.com>
2026-01-20 17:33:08 -0800

    test-linux works

--- build.zig
+++ build.zig
@@ -23,7 +23,7 @@ pub fn build(b: *std.Build) void {
     });
     b.installArtifact(linux_exe);
 
-    // Build and install zig tests for running on host
+    // Build zig tests for running on host
     const host_tests = b.addTest(.{
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
@@ -31,9 +31,8 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
         }),
     });
-    b.installArtifact(host_tests);
 
-    // Build and install zig tests for linux running in docker
+    // Build and install zig tests for running in linux container
     const linux_tests = b.addTest(.{
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
@@ -41,27 +40,25 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
         }),
     });
-    b.installArtifact(linux_tests);
-
-    // Add CLI commands for build.zig
+    const test_install = b.addInstallArtifact(linux_tests, .{ .dest_sub_path = "tests" });
+    b.getInstallStep().dependOn(&test_install.step);
 
     // 'run' mounts built linux exe into a linux container and runs it there
-    const cli_step = b.step("run", "Run executable in a Linux container");
-    const runner_args = [_][]const u8{ "docker", "run", "--rm", "-v", "./zig-out:/zig-out", "alpine", "/zig-out/bin/bVisor" };
-    const runner = b.addSystemCommand(&runner_args);
-    var runner_step = runner.step;
-    runner_step.dependOn(b.getInstallStep()); // docker run command depends on linux exe having been built
-    cli_step.dependOn(&runner_step); // ensure run command
+    const run_cli_step = b.step("run", "Run executable in a Linux container");
+    const run_args = [_][]const u8{ "docker", "run", "--rm", "-v", "./zig-out:/zig-out", "alpine", "/zig-out/bin/bVisor" };
+    const run_cmd = b.addSystemCommand(&run_args);
+    run_cmd.step.dependOn(b.getInstallStep()); // docker run depends on linux exe being built
+    run_cli_step.dependOn(&run_cmd.step);
 
-    // Build unit tests for host target
-    const host_test_step = b.step("test", "Run unit tests on host");
-    const host_unit_tests = b.addTest(.{
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = host_target,
-            .optimize = optimize,
-        }),
-    });
-    const host_test_run = b.addRunArtifact(host_unit_tests);
-    host_test_step.dependOn(&host_test_run.step);
+    // 'test' runs unit tests on host
+    const test_cli_step = b.step("test", "Run unit tests on host");
+    const test_cmd = b.addRunArtifact(host_tests);
+    test_cli_step.dependOn(&test_cmd.step);
+
+    // 'test-linux' runs unit tests in linux container
+    const test_linux_cli_step = b.step("test-linux", "Run unit tests in linux container");
+    const test_linux_args = [_][]const u8{ "docker", "run", "--rm", "-v", "./zig-out:/zig-out", "alpine", "/zig-out/bin/bVisor-test" };
+    const test_linux_cmd = b.addSystemCommand(&test_linux_args);
+    test_linux_cmd.step.dependOn(b.getInstallStep()); // docker run depends on linux tests being built
+    test_linux_cli_step.dependOn(&test_linux_cmd.step);
 }
